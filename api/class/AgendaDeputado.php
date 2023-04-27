46b3827c6daebf2b191aea473594769cc0fc53e4
JairoTSantos <107869809+JairoTSantos@users.noreply.github.com>
2023-04-27 12:34:25 -0300

    Update AgendaDeputado.php

--- api/class/AgendaDeputado.php
+++ api/class/AgendaDeputado.php
@@ -11,8 +11,14 @@ class AgendaDeputado {
         return $this->data->Query('INSERT INTO db_agenda (agenda_situacao, agenda_data, agenda_titulo, agenda_local, agenda_equipe, agenda_presenca, agenda_uf, agenda_tipo) VALUES ("' . $situacao . '", "' . $horario . ' ' . $data . ':00", "' . $titulo . '", "' . $local . '", "' . $equipe . '", "' . $presenca . '", "' . $uf . '", "' . $tipo . '")');
     }
 
+    
+    
     public function atualizarCompromisso($id, $situacao, $data, $horario, $titulo, $local, $equipe, $presenca, $uf, $tipo) {
-        return $this->data->Query('UPDATE db_agenda SET agenda_situacao="' . $situacao . '", agenda_data="' . $data . ' ' . $horario . '", agenda_titulo="' . $titulo . '", agenda_local="' . $local . '", agenda_equipe="' . $equipe . '", agenda_presenca="' . $presenca . '", agenda_uf="' . $uf . '", agenda_tipo="' . $tipo . '" WHERE agenda_id = ' . $id . ';');
+        if (count($this->data->Buscar('SELECT agenda_id FROM db_agenda WHERE agenda_id = ' . $id . ';')) > 0) {
+            return $this->data->Query('UPDATE db_agenda SET agenda_situacao="' . $situacao . '", agenda_data="' . $data . ' ' . $horario . '", agenda_titulo="' . $titulo . '", agenda_local="' . $local . '", agenda_equipe="' . $equipe . '", agenda_presenca="' . $presenca . '", agenda_uf="' . $uf . '", agenda_tipo="' . $tipo . '" WHERE agenda_id = ' . $id . ';');
+        }else{
+            return false;
+        } 
     }
 
     public function atualizarStatus($id, $situacao) {
